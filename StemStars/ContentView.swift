54e72a285daf75c34a1ded13c680d57d02cb84f2
ZaiCole1406 <lalarose1406@gmail.com>
2023-08-10 13:13:37 -0400

    Initial Commit

---
+++ StemStars/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  StemStars
+//
+//  Created by scholar on 18/05/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            
+            Color(.systemRed)
+                .ignoresSafeArea()
+                
+            VStack(alignment: .leading, spacing: 20.0) {
+                
+                Image("Joy StemStars")
+                
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(100)
+                HStack{
+                    Text("Joy Buolamwini")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        
+                    Text("Poet of Code")
+                }
+                
+                Text("Founder of the Algorithmic Justice League. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology.")
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+            }
+            .background(Rectangle().foregroundColor(.white))
+            .cornerRadius(25)
+            .shadow(radius : 25)
+            .padding()
+           
+            
+            
+            
+        }
+        
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StemStars/StemStarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StemStarsApp.swift
+//  StemStars
+//
+//  Created by scholar on 18/05/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct StemStarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
